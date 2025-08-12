4364337aa8904209f83d7389b0b75d6075b27a83
Mrigangka Datta <mrigangkadatta15@gmail.com>
2025-08-12 16:59:30 +0530

    configured axiosfor overall access

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const baseUrl = import.meta.env.VITE_BASE_API_URL;
+
+
+const axiosInstance = axios.create({
+    baseURL:baseUrl,
+    timeout:1000,
+    headers:{
+        'Content-Type':'application/json'
+    },
+    withCredentials:true
+});
+
+
+export default axiosInstance
